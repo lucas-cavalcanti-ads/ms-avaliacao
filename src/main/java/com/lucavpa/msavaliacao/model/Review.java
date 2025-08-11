c9fee10969f502c1e95d481912db1f76fd1ed2cd
lucas-cavalcanti-ads <lucas.tnv27@gmail.com>
2025-08-11 02:02:14 -0300

    feat: refatoracao e inclusao de novas funcionalidades ao review

--- src/main/java/com/lucavpa/msavaliacao/model/Review.java
+++ src/main/java/com/lucavpa/msavaliacao/model/Review.java
@@ -1,26 +1,45 @@
 package com.lucavpa.msavaliacao.model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.*;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "tb_review")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Review {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
-    @Column(name = "person_id")
-    Long personId;
+    @NotNull(message = "O ID do restaurante é obrigatório.")
+    @Column(name = "restaurant_id", nullable = false)
+    private Long restaurantId;
 
-    @Column(name = "order_number")
-    int orderNumber;
+    @NotNull(message = "O ID do cliente é obrigatório.")
+    @Column(name = "person_id", nullable = false)
+    private Long personId;
 
-    @Column(name = "comment", length = 100)
-    String comment;
+    @NotNull(message = "A nota da avaliação é obrigatório.")
+    @Column(name = "order_number", nullable = false)
+    private int orderNumber;
+
+    @Column(name = "comment", length = 100, nullable = false)
+    private String comment;
+
+    @CreatedDate
+    @Column(name="create_date", updatable = false, nullable = false)
+    private LocalDateTime createDate;
 
 }
--- src/main/java/com/lucavpa/msavaliacao/service/ReviewService.java
+++ src/main/java/com/lucavpa/msavaliacao/service/ReviewService.java
@@ -1,32 +1,52 @@
 package com.lucavpa.msavaliacao.service;
 
-import com.lucavpa.msavaliacao.model.Review;
+import com.lucavpa.msavaliacao.exception.ReviewNotFoundException;
+import com.lucavpa.msavaliacao.handler.ApiExceptionHandler;
 import com.lucavpa.msavaliacao.repository.ReviewRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.lucavpa.msavaliacao.model.Review;
 
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
 
-    @Autowired
-    private ReviewRepository repository;
+    private final ReviewRepository repository;
+
+    private static final Logger logger = LogManager.getLogger(ApiExceptionHandler.class);
+
+    public ReviewService (ReviewRepository reviewRepository) {
+        this.repository = reviewRepository;
+    }
 
     public Review save (Review review){
-        System.out.println("[LOG][SERVICE] Method save");
-        return repository.save(review);
+
+        Review reviewSaved = repository.save(review);
+
+        logger.info("[SERVICE][REVIEWS] Salvamento da avaliação finalizado com sucesso.");
+
+        return reviewSaved;
     }
 
     public List<Review> getAll () {
-        System.out.println("[LOG][SERVICE] getAll");
-        return repository.findAll();
+        List<Review> reviewListConsulted = repository.findAll();
+
+        logger.info("[SERVICE][REVIEWS] Consulta a lista de todas as avaliações finalizada com sucesso.");
+
+        return reviewListConsulted;
     }
 
     public Review getById (Long id) {
-        System.out.println("[LOG][SERVICE] getAll");
-        return repository.findById(id).get();
+
+        Review reviewByIdConsulted = repository.findById(id)
+                .orElseThrow(() -> new ReviewNotFoundException(id));
+
+        logger.info("[SERVICE][REVIEWS] Consulta a lista de todas as avaliações por ID finalizada com sucesso.");
+
+        return reviewByIdConsulted;
     }
 
 }
