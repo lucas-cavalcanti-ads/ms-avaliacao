623ca45324a5719890f71db4a211a6ff66125cd5
lucas-cavalcanti-ads <lucas.tnv27@gmail.com>
2025-08-11 02:02:53 -0300

    feat: inclusao da funcionalidade de media das avaliacoes

---
+++ src/main/java/com/lucavpa/msavaliacao/model/ReviewAverageDto.java
@@ -0,0 +1,19 @@
+package com.lucavpa.msavaliacao.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class ReviewAverageDto {
+
+    private Long restaurantId;
+
+    private int amountReviews;
+
+    private double average;
+
+
+}
