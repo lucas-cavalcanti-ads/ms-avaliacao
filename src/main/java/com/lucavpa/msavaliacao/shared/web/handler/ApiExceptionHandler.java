7f398ec8f78dc82504cbabd7e675db97ff8f3110
lucas-cavalcanti-ads <lucas.tnv27@gmail.com>
2025-08-16 20:39:43 -0300

    refactor: Ajuste do Handler dado a validacao do Body na controller

--- src/main/java/com/lucavpa/msavaliacao/shared/web/handler/ApiExceptionHandler.java
+++ src/main/java/com/lucavpa/msavaliacao/shared/web/handler/ApiExceptionHandler.java
@@ -1,17 +1,12 @@
 package com.lucavpa.msavaliacao.shared.web.handler;
 
-import com.lucavpa.msavaliacao.review.domain.exception.ReviewNotFoundException;
-
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.ConstraintViolationException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -21,43 +16,24 @@ public class ApiExceptionHandler {
 
     private static final Logger logger = LogManager.getLogger(ApiExceptionHandler.class);
 
-    record AtributesError (String propertyPath, String messageTemplate){}
-
-    @ExceptionHandler(ReviewNotFoundException.class)
-    public ResponseEntity<String> handlerReviewNotFound(
-            ReviewNotFoundException exception, HttpServletRequest request) {
-
-        Map<String, Object> body = Map.of(
-            "title", "Recurso não encontrado",
-            "status", 404,
-            "detail", exception.getMessage(),
-            "uri", request.getRequestURI()
-        );
-
-        logger.warn("[HANDLER][REVIEWS] Avaliação não encontrada " + body);
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List<AtributesError>> handlerConstraintViolation(
-            final ConstraintViolationException exception, final HttpServletRequest request) {
-
-        var errors = exception.getConstraintViolations().stream()
-                        .map(indice -> new AtributesError(
-                            indice.getPropertyPath().toString(),
-                            indice.getMessageTemplate()))
-                        .toList();
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<Map<String,String>>> handleBodyErrors(MethodArgumentNotValidException ex) {
+        var errors = ex.getBindingResult().getFieldErrors().stream()
+                .map(e -> Map.of(
+                        "propriedade", e.getField(),
+                        "mensagem", e.getDefaultMessage()))
+                .toList();
 
         Map<String, Object> body = Map.of(
                 "title", "Requisição inválida",
                 "status", 400,
-                "detail", exception.getConstraintViolations(),
-                "uri", request.getRequestURI()
+                "detail", errors,
+                "messagw", ex.getBindingResult()
         );
 
-        logger.warn("[HANDLER][REVIEWS] Requisição inválida " + body);
+        logger.warn("[HANDLER][handleBodyErrors] Requisição inválida " + body);
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        return ResponseEntity.badRequest().body(errors);
     }
+
 }
