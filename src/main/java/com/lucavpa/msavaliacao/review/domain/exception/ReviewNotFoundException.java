55bbdb4954be640ff8b17609d527321f70f017f3
lucas-cavalcanti-ads <lucas.tnv27@gmail.com>
2025-08-16 20:19:21 -0300

    style: Ajuste de espaco da mensagem da exception ReviewNotFoundException

--- src/main/java/com/lucavpa/msavaliacao/review/domain/exception/ReviewNotFoundException.java
+++ src/main/java/com/lucavpa/msavaliacao/review/domain/exception/ReviewNotFoundException.java
@@ -3,6 +3,6 @@ package com.lucavpa.msavaliacao.review.domain.exception;
 public class ReviewNotFoundException extends RuntimeException{
 
     public ReviewNotFoundException (final Long idReview) {
-        super("Avaliação de ID  " + idReview + ", não encontrada.");
+        super("Avaliação de ID " + idReview + ", não encontrada.");
     }
 }
