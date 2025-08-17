7b95858fa00559be133924165f4d90c604b4fae8
lucas-cavalcanti-ads <lucas.tnv27@gmail.com>
2025-08-17 00:49:54 -0300

    feat: criacao do tratamento da consulta de avaliacoes sem nenhuma avaliacao

--- src/main/java/com/lucavpa/msavaliacao/review/app/service/ReviewService.java
+++ src/main/java/com/lucavpa/msavaliacao/review/app/service/ReviewService.java
@@ -1,9 +1,13 @@
 package com.lucavpa.msavaliacao.review.app.service;
 
+import com.lucavpa.msavaliacao.restaurant.domain.exception.RestaurantNotFoundException;
+import com.lucavpa.msavaliacao.review.domain.exception.ReviewNotFoundByIdException;
 import com.lucavpa.msavaliacao.review.domain.exception.ReviewNotFoundException;
 import com.lucavpa.msavaliacao.review.infra.repository.ReviewRepository;
 import com.lucavpa.msavaliacao.review.domain.model.Review;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -32,22 +36,31 @@ public class ReviewService {
         return reviewSaved;
     }
 
-    public List<Review> getAll () {
+    public List<Review> getAll () throws Exception {
         List<Review> reviewListConsulted = repository.findAll();
 
-        logger.info("[SERVICE][REVIEWS] Consulta a lista de todas as avaliações finalizada com sucesso.");
+        if (this.haveReviewList(reviewListConsulted)){
+            logger.info("[SERVICE][REVIEWS] Consulta a lista de todas as avaliações finalizada com sucesso.");
+            return reviewListConsulted;
+        }
 
-        return reviewListConsulted;
+        return Collections.emptyList();
+
     }
 
     public Review getById (Long id) {
 
         Review reviewByIdConsulted = repository.findById(id)
-                .orElseThrow(() -> new ReviewNotFoundException(id));
+                .orElseThrow(() -> new ReviewNotFoundByIdException(id));
 
         logger.info("[SERVICE][REVIEWS] Consulta a lista de todas as avaliações por ID finalizada com sucesso.");
 
         return reviewByIdConsulted;
+    }
+
+    private boolean haveReviewList (List<Review> reviewList) throws Exception{
+        if(reviewList.size() > 0) return true;
+        throw new ReviewNotFoundException();
     }
 
 }
